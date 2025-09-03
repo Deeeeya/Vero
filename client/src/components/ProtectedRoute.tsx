098984f39f9f4ee65cb184af30fa188b4af45258
Deeeeya <tankonated@gmail.com>
2025-09-03 01:58:08 -0400

    feat(ProtectedRoute.tsx): added component that checks to see if user is authenticated, else they cannot access dev tools

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  // Expects childre prop
+  children: ReactNode; // This tells TypeScript what props this component expects and protected page will be wrapped
+}
+
+export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { isAuthenticated, isLoading } = useAuth(); // Gets current login status from context
+
+  if (isLoading == true) {
+    // 'While checking authentication status'
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated == false) {
+    // 'If user is not logged in' it will redirect page
+    return <Navigate to="/login" replace />; // 'replace' preventss back button issues
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
